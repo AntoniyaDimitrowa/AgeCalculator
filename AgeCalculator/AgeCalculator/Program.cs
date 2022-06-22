483adee6010095ba016e4f83001eba725fe2d795
AntoniyaDimitrowa <antsimdim04@gmail.com>
2022-06-22 23:32:14 +0300

    Added file

---
+++ AgeCalculator/AgeCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AgeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input year of birth: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input month of birth(1,2...12): ");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input day of birth: ");
+            int day = int.Parse(Console.ReadLine());
+
+            DateTime now = DateTime.Now;
+
+            int todaysYear = now.Year;
+            int todaysDay = now.Day;
+            int todaysMonth = now.Month;
+
+            int kidsYears = 0;
+            int kidsMonths = 0;
+            int kidsDays = 0;
+
+            int daysInFebruary;
+            if (year % 4 == 0)
+            {
+                daysInFebruary = 29;
+            }
+            else { daysInFebruary = 28; }
+            int[] daysInMonth = new int[12] { 31, daysInFebruary, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        }
+    }
+}
